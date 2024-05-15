409f040a3b85b776e3296a5c83a144a6a9508417
koudaimurakami <hakmd.m.98@gmail.com>
2024-05-15 21:49:11 +0900

    wip

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -2,4 +2,24 @@ class UserAnswersController < ApplicationController
   def new
     @user_answer = UserAnswer.new
   end
+  
+  def create
+    (0..9).each do |index|
+      user_answer_params = params[:user_answer]["user_answer_#{index}"]
+      user_answer = UserAnswer.new(user_id: user_answer_params[:user_id],
+                                   category_quiz_question_id: user_answer_params[:category_quiz_question_id],
+                                   category_question_choice_id: user_answer_params[:category_question_choice_id])
+      user_answer.save
+    end
+    redirect_to user_answers_path
+  end
+
+  def index
+    @user_answers = UserAnswer.where(user_id: current_user.id)
+  end
+  
+  private
+
+  
+  
 end
