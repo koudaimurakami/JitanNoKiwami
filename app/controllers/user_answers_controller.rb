3932da04e8c064a6f601be0137f86e215accede3
koudaimurakami <hakmd.m.98@gmail.com>
2024-05-16 23:54:25 +0900

    ユーザーの回答の保存処理修正

--- app/controllers/user_answers_controller.rb
+++ app/controllers/user_answers_controller.rb
@@ -6,13 +6,15 @@ class UserAnswersController < ApplicationController
   def create
     session_identifier = params[:user_answer][:session_identifier]
     
-    (0..9).each do |index|
-      user_answer_params = params[:user_answer]["user_answer_#{index}"]
-      user_answer = UserAnswer.new(user_id: user_answer_params[:user_id],
-                                   category_quiz_question_id: user_answer_params[:category_quiz_question_id],
-                                   category_question_choice_id: user_answer_params[:category_question_choice_id],
-                                   session_identifier: session_identifier)
-      user_answer.save
+    ActiveRecord::Base.transaction do
+      (0..9).each do |index|
+        user_answer_params = params[:user_answer]["user_answer_#{index}"]
+        user_answer = UserAnswer.new(user_id: user_answer_params[:user_id],
+                                     category_quiz_question_id: user_answer_params[:category_quiz_question_id],
+                                     category_question_choice_id: user_answer_params[:category_question_choice_id],
+                                     session_identifier: session_identifier)
+        user_answer.save!
+      end
     end
     redirect_to user_answers_path(session_identifier: session_identifier)
   end
